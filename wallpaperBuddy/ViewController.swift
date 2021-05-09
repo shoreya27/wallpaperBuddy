4c492e8040595897978b1671598b9599634c8ba1
shoreya <gupta.shoreya2707@gmail.com>
2021-05-10 00:18:29 +0530

    My Own Wallpaper Buddy App

--- wallpaperBuddy/ViewController.swift
+++ wallpaperBuddy/ViewController.swift
@@ -9,11 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var backgroundImage: UIImageView!
+    let backgroundArray = [#imageLiteral(resourceName: "one"),#imageLiteral(resourceName: "two"),#imageLiteral(resourceName: "three"),#imageLiteral(resourceName: "four"),#imageLiteral(resourceName: "five"),#imageLiteral(resourceName: "six"),#imageLiteral(resourceName: "seven")]
+    //var counter = 0
+    @IBAction func hitMe(_ sender: UIButton) {
+        backgroundImage.image = backgroundArray.randomElement()!
+        //counter += 1
     }
-
-
+    
 }
 
